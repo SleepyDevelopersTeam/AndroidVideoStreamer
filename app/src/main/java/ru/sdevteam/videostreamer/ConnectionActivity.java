363a5b4d80e5da65e2fe8726fbdc93de4c3125f3
Nikita <nikita-ivanov.mail@yandex.ru>
2016-08-23 21:21:31 +0300

    First commit

---
+++ app/src/main/java/ru/sdevteam/videostreamer/ConnectionActivity.java
@@ -0,0 +1,20 @@
+package ru.sdevteam.videostreamer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ConnectionActivity extends AppCompatActivity
+{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_connection);
+	}
+
+	private void connect()
+	{
+		
+	}
+}
